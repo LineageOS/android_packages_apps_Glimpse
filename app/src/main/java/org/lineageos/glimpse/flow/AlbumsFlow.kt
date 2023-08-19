luca020400/android_packages_apps_Gallery

Glimpse: Include the thumbnail media in album
Change-Id: I5041f0c947d6645447e3c0480ca364fb3552cdf2

app/src/main/java/org/lineageos/glimpse/flow/AlbumsFlow.kt
@@ -6,18 +6,19 @@
 package org.lineageos.glimpse.flow
 
 import android.content.ContentResolver
-import android.content.ContentUris
 import android.content.Context
 import android.database.Cursor
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import androidx.core.database.getStringOrNull
 import androidx.core.os.bundleOf
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import org.lineageos.glimpse.R
 import org.lineageos.glimpse.ext.queryFlow
 import org.lineageos.glimpse.models.Album
+import org.lineageos.glimpse.models.Media
 import org.lineageos.glimpse.query.*
 import org.lineageos.glimpse.utils.MediaStoreBuckets
 
@@ -51,35 +52,42 @@ class AlbumsFlow(private val context: Context) : QueryFlow<Album>() {
     override fun flowData() = flowCursor().map {
         mutableMapOf<Int, Album>().apply {
             it?.use {
-                val idIndex = it.getColumnIndex(MediaStore.Files.FileColumns._ID)
-                val isFavoriteIndex =
-                    it.getColumnIndex(MediaStore.Files.FileColumns.IS_FAVORITE)
-                val isTrashedIndex = it.getColumnIndex(MediaStore.Files.FileColumns.IS_TRASHED)
-                val mediaTypeIndex = it.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE)
-                val bucketIdIndex = it.getColumnIndex(MediaStore.Files.FileColumns.BUCKET_ID)
                 val bucketDisplayNameIndex =
                     it.getColumnIndex(MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME)
+                val idIndex = it.getColumnIndex(MediaStore.Files.FileColumns._ID)
+                val bucketIdIndex = it.getColumnIndex(MediaStore.MediaColumns.BUCKET_ID)
+                val isFavoriteIndex = it.getColumnIndex(MediaStore.Files.FileColumns.IS_FAVORITE)
+                val isTrashedIndex = it.getColumnIndex(MediaStore.Files.FileColumns.IS_TRASHED)
+                val mediaTypeIndex = it.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE)
+                val mimeTypeIndex = it.getColumnIndex(MediaStore.Files.FileColumns.MIME_TYPE)
+                val dateAddedIndex = it.getColumnIndex(MediaStore.Files.FileColumns.DATE_ADDED)
+                val dateModifiedIndex =
+                    it.getColumnIndex(MediaStore.Files.FileColumns.DATE_MODIFIED)
+                val orientationIndex = it.getColumnIndex(MediaStore.Files.FileColumns.ORIENTATION)
 
                 if (!it.moveToFirst()) {
                     return@use
                 }
 
                 while (!it.isAfterLast) {
-                    val contentUri = when (it.getInt(mediaTypeIndex)) {
-                        MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE -> MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-
-                        MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO -> MediaStore.Video.Media.EXTERNAL_CONTENT_URI
-
-                        else -> continue
-                    }
+                    val bucketDisplayName = it.getStringOrNull(bucketDisplayNameIndex)
+                    val id = it.getLong(idIndex)
+                    val buckedId = it.getInt(bucketIdIndex)
+                    val isFavorite = it.getInt(isFavoriteIndex)
+                    val isTrashed = it.getInt(isTrashedIndex)
+                    val mediaType = it.getInt(mediaTypeIndex)
+                    val mimeType = it.getString(mimeTypeIndex)
+                    val dateAdded = it.getLong(dateAddedIndex)
+                    val dateModified = it.getLong(dateModifiedIndex)
+                    val orientation = it.getInt(orientationIndex)
 
                     val bucketIds = listOfNotNull(
-                        when (it.getInt(isTrashedIndex)) {
+                        when (isTrashed) {
                             1 -> MediaStoreBuckets.MEDIA_STORE_BUCKET_TRASH.id
-                            else -> it.getInt(bucketIdIndex)
+                            else -> buckedId
                         },
                         MediaStoreBuckets.MEDIA_STORE_BUCKET_FAVORITES.id.takeIf { _ ->
-                            it.getInt(isFavoriteIndex) == 1
+                            isFavorite == 1
                         },
                     )
 
@@ -87,27 +95,35 @@ class AlbumsFlow(private val context: Context) : QueryFlow<Album>() {
                         this[bucketId]?.also { album ->
                             album.size += 1
                         } ?: run {
-                                val thumbnailId = it.getLong(idIndex)
-                                this[bucketId] = Album(
-                                    bucketId,
-                                    when (bucketId) {
-                                        MediaStoreBuckets.MEDIA_STORE_BUCKET_FAVORITES.id -> context.getString(
-                                            R.string.album_favorites
-                                        )
+                            this[bucketId] = Album(
+                                bucketId,
+                                when (bucketId) {
+                                    MediaStoreBuckets.MEDIA_STORE_BUCKET_FAVORITES.id -> context.getString(
+                                        R.string.album_favorites
+                                    )
 
-                                        MediaStoreBuckets.MEDIA_STORE_BUCKET_TRASH.id -> context.getString(
-                                            R.string.album_trash
-                                        )
+                                    MediaStoreBuckets.MEDIA_STORE_BUCKET_TRASH.id -> context.getString(
+                                        R.string.album_trash
+                                    )
 
-                                        else -> it.getString(bucketDisplayNameIndex) ?: Build.MODEL
-                                    },
-                                    ContentUris.withAppendedId(contentUri, thumbnailId),
-                                    thumbnailId,
-                                ).apply { size += 1 }
-                            }
+                                    else -> bucketDisplayName ?: Build.MODEL
+                                },
+                                Media.fromMediaStore(
+                                    id,
+                                    buckedId,
+                                    isFavorite,
+                                    isTrashed,
+                                    mediaType,
+                                    mimeType,
+                                    dateAdded,
+                                    dateModified,
+                                    orientation,
+                                )
+                            ).apply { size += 1 }
                         }
-                        it.moveToNext()
                     }
+                    it.moveToNext()
+                }
                 }
             }.values.toList()
         }

app/src/main/java/org/lineageos/glimpse/models/Album.kt
@@ -5,7 +5,6 @@
 
 package org.lineageos.glimpse.models
 
-import android.net.Uri
 import android.os.Parcel
 import android.os.Parcelable
 import org.lineageos.glimpse.ext.*
@@ -17,15 +16,13 @@ import kotlin.reflect.safeCast
 data class Album(
     val id: Int,
     val name: String,
-    val thumbnail: Uri,
-    val thumbnailId: Long,
+    val media: Media,
     var size: Int = 0,
 ) : Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readInt(),
         parcel.readString()!!,
-        parcel.readParcelable(Uri::class)!!,
-        parcel.readLong(),
+        parcel.readParcelable(Media::class)!!,
         parcel.readInt()
     )
 
@@ -36,8 +33,7 @@ data class Album(
             obj,
             { it.id },
             { it.name },
-            { it.thumbnail },
-            { it.thumbnailId },
+            { it.media },
             { it.size },
         ) == 0
     }
@@ -49,8 +45,7 @@ data class Album(
     override fun writeToParcel(dest: Parcel, flags: Int) {
         dest.writeInt(id)
         dest.writeString(name)
-        dest.writeParcelable(thumbnail, 0)
-        dest.writeLong(thumbnailId)
+        dest.writeParcelable(media, 0)
         dest.writeInt(size)
     }
 

app/src/main/java/org/lineageos/glimpse/thumbnail/AlbumThumbnailAdapter.kt
@@ -75,8 +75,8 @@ class AlbumThumbnailAdapter(
                 R.plurals.album_thumbnail_items, album.size, album.size
             )
 
-            thumbnailImageView.load(album.thumbnail) {
-                memoryCacheKey("thumbnail_${album.thumbnailId}")
+            thumbnailImageView.load(album.media.externalContentUri) {
+                memoryCacheKey("thumbnail_${album.media.id}")
                 size(ThumbnailLayoutManager.MAX_THUMBNAIL_SIZE.px)
                 placeholder(R.drawable.thumbnail_placeholder)
             }

LineageOS/android_packages_apps_Glimpse

app/src/main/java/org/lineageos/glimpse/flow/AlbumsFlow.kt
@@ -6,7 +6,6 @@
 package org.lineageos.glimpse.flow
 
 import android.content.ContentResolver
-import android.content.ContentUris
 import android.content.Context
 import android.database.Cursor
 import android.os.Build
@@ -18,6 +17,7 @@ import kotlinx.coroutines.flow.map
 import org.lineageos.glimpse.R
 import org.lineageos.glimpse.ext.queryFlow
 import org.lineageos.glimpse.models.Album
+import org.lineageos.glimpse.models.Media
 import org.lineageos.glimpse.query.*
 import org.lineageos.glimpse.utils.MediaStoreBuckets
 
@@ -52,11 +52,13 @@ class AlbumsFlow(private val context: Context) : QueryFlow<Album>() {
         mutableMapOf<Int, Album>().apply {
             it?.use {
                 val idIndex = it.getColumnIndex(MediaStore.Files.FileColumns._ID)
+                val bucketIdIndex = it.getColumnIndex(MediaStore.Files.FileColumns.BUCKET_ID)
                 val isFavoriteIndex =
                     it.getColumnIndex(MediaStore.Files.FileColumns.IS_FAVORITE)
                 val isTrashedIndex = it.getColumnIndex(MediaStore.Files.FileColumns.IS_TRASHED)
                 val mediaTypeIndex = it.getColumnIndex(MediaStore.Files.FileColumns.MEDIA_TYPE)
-                val bucketIdIndex = it.getColumnIndex(MediaStore.Files.FileColumns.BUCKET_ID)
+                val mimeTypeIndex = it.getColumnIndex(MediaStore.Files.FileColumns.MIME_TYPE)
+                val dateAddedIndex = it.getColumnIndex(MediaStore.Files.FileColumns.DATE_ADDED)
                 val bucketDisplayNameIndex =
                     it.getColumnIndex(MediaStore.Files.FileColumns.BUCKET_DISPLAY_NAME)
 
@@ -65,13 +67,7 @@ class AlbumsFlow(private val context: Context) : QueryFlow<Album>() {
                 }
 
                 while (!it.isAfterLast) {
-                    val contentUri = when (it.getInt(mediaTypeIndex)) {
-                        MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE -> MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-
-                        MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO -> MediaStore.Video.Media.EXTERNAL_CONTENT_URI
-
-                        else -> continue
-                    }
+                    val bucketId = it.getInt(bucketIdIndex)
 
                     val bucketIds = listOfNotNull(
                         when (it.getInt(isTrashedIndex)) {
@@ -83,13 +79,21 @@ class AlbumsFlow(private val context: Context) : QueryFlow<Album>() {
                         },
                     )
 
-                    for (bucketId in bucketIds) {
-                        this[bucketId]?.also { album ->
+                    for (displayedBucketId in bucketIds) {
+                        this[displayedBucketId]?.also { album ->
                             album.size += 1
                         } ?: run {
-                            this[bucketId] = Album(
-                                bucketId,
-                                when (bucketId) {
+                            val id = it.getLong(idIndex)
+                            val isFavorite = it.getInt(isFavoriteIndex)
+                            val isTrashed = it.getInt(isTrashedIndex)
+                            val mediaType = it.getInt(mediaTypeIndex)
+                            val mimeType = it.getString(mimeTypeIndex)
+                            val dateAdded = it.getLong(dateAddedIndex)
+                            val bucketDisplayName = it.getString(bucketDisplayNameIndex)
+
+                            this[displayedBucketId] = Album(
+                                displayedBucketId,
+                                when (displayedBucketId) {
                                     MediaStoreBuckets.MEDIA_STORE_BUCKET_FAVORITES.id -> context.getString(
                                         R.string.album_favorites
                                     )
@@ -98,9 +102,17 @@ class AlbumsFlow(private val context: Context) : QueryFlow<Album>() {
                                         R.string.album_trash
                                     )
 
-                                    else -> it.getString(bucketDisplayNameIndex) ?: Build.MODEL
+                                    else -> bucketDisplayName ?: Build.MODEL
                                 },
-                                ContentUris.withAppendedId(contentUri, it.getLong(idIndex)),
+                                Media.fromMediaStore(
+                                    id,
+                                    bucketId,
+                                    isFavorite,
+                                    isTrashed,
+                                    mediaType,
+                                    mimeType,
+                                    dateAdded,
+                                )
                             ).apply { size += 1 }
                         }
                     }

app/src/main/java/org/lineageos/glimpse/models/Album.kt
@@ -5,7 +5,6 @@
 
 package org.lineageos.glimpse.models
 
-import android.net.Uri
 import android.os.Parcel
 import android.os.Parcelable
 import org.lineageos.glimpse.ext.*
@@ -17,29 +16,30 @@ import kotlin.reflect.safeCast
 data class Album(
     val id: Int,
     val name: String,
-    val thumbnail: Uri,
+    val thumbnail: Media,
     var size: Int = 0,
-) : Parcelable {
+) : Comparable<Album>, Parcelable {
     constructor(parcel: Parcel) : this(
         parcel.readInt(),
         parcel.readString()!!,
-        parcel.readParcelable(Uri::class)!!,
+        parcel.readParcelable(Media::class)!!,
         parcel.readInt()
     )
 
     override fun equals(other: Any?): Boolean {
         val obj = Album::class.safeCast(other) ?: return false
-        return compareValuesBy(
-            this,
-            obj,
-            { it.id },
-            { it.name },
-            { it.thumbnail },
-            { it.size },
-        ) == 0
+        return compareTo(obj) == 0
     }
 
     override fun hashCode() = id.hashCode()
+
+    override fun compareTo(other: Album) = compareValuesBy(
+        this, other,
+        { it.id },
+        { it.name },
+        { it.thumbnail },
+        { it.size },
+    )
 
     override fun describeContents() = 0
 

app/src/main/java/org/lineageos/glimpse/thumbnail/AlbumThumbnailAdapter.kt
@@ -70,7 +70,7 @@ class AlbumThumbnailAdapter(
                 R.plurals.album_thumbnail_items, album.size, album.size
             )
 
-            thumbnailImageView.load(album.thumbnail) {
+            thumbnailImageView.load(album.thumbnail.externalContentUri) {
                 size(ThumbnailLayoutManager.MAX_THUMBNAIL_SIZE.px)
                 placeholder(R.drawable.thumbnail_placeholder)
             }