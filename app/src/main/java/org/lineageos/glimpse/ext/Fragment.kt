f805d0ce4a74060539f0ac11a9272982c82d5498
Luca Stefani <luca.stefani.ge1@gmail.com>
2023-09-03 16:30:27 +0200

    Glimpse: Cleanup getViewProperty delegate
    Change-Id: I2f3cf5df7ddcbc3353b2733c289fd8d155c99385

--- app/src/main/java/org/lineageos/glimpse/ext/Fragment.kt
+++ app/src/main/java/org/lineageos/glimpse/ext/Fragment.kt
@@ -8,15 +8,9 @@ package org.lineageos.glimpse.ext
 import android.view.View
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
-import kotlin.reflect.KProperty
+import kotlin.properties.ReadOnlyProperty
 
-class FragmentViewProperty<T : View?>(
-    private val fragment: Fragment, @IdRes private val viewId: Int
-) {
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
-        return fragment.requireView().findViewById<T>(viewId)
+inline fun <reified T : View?> getViewProperty(@IdRes viewId: Int) =
+    ReadOnlyProperty<Fragment, T> { thisRef, _ ->
+        thisRef.requireView().findViewById<T>(viewId)
     }
-}
-
-fun <T : View?> Fragment.getViewProperty(@IdRes viewId: Int) =
-    FragmentViewProperty<T>(this, viewId)
