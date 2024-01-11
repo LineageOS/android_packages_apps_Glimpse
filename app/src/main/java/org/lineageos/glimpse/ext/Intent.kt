bf4d15c4fecb26341f2332f2e7b66e02649be9ff
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-01-11 16:23:10 +0100

    Glimpse: Commonize buildShareIntent for all Media implementations
    Change-Id: I6ef49b9737513ec653572b1f817d00cc0cb060b3

--- app/src/main/java/org/lineageos/glimpse/ext/Intent.kt
+++ app/src/main/java/org/lineageos/glimpse/ext/Intent.kt
@@ -6,12 +6,12 @@
 package org.lineageos.glimpse.ext
 
 import android.content.Intent
+import org.lineageos.glimpse.models.Media
 import org.lineageos.glimpse.models.MediaStoreMedia
 import org.lineageos.glimpse.models.MediaType.IMAGE
 import org.lineageos.glimpse.models.MediaType.VIDEO
-import org.lineageos.glimpse.models.UriMedia
 
-fun buildShareIntent(vararg medias: MediaStoreMedia) = Intent().apply {
+fun buildShareIntent(vararg medias: Media) = Intent().apply {
     assert(medias.isNotEmpty()) { "No media" }
 
     if (medias.size == 1) {
@@ -36,32 +36,6 @@ fun buildShareIntent(vararg medias: MediaStoreMedia) = Intent().apply {
         }
     }
     flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-}
-
-fun buildShareIntent(vararg uriMedias: UriMedia) = Intent().apply {
-    assert(uriMedias.isNotEmpty()) { "No media uri" }
-
-    if (uriMedias.size == 1) {
-        action = Intent.ACTION_SEND
-        uriMedias[0].let {
-            putExtra(Intent.EXTRA_STREAM, it.uri)
-            type = it.mimeType
-        }
-    } else {
-        action = Intent.ACTION_SEND_MULTIPLE
-        putParcelableArrayListExtra(
-            Intent.EXTRA_STREAM,
-            uriMedias.map { it.uri }.toCollection(ArrayList())
-        )
-        type = when {
-            uriMedias.all { it.mediaType == IMAGE } -> "image/*"
-            uriMedias.all { it.mediaType == VIDEO } -> "video/*"
-            else -> {
-                putExtra(Intent.EXTRA_MIME_TYPES, arrayOf("image/*", "video/*"))
-                "*/*"
-            }
-        }
-    }
 }
 
 fun buildEditIntent(media: MediaStoreMedia) = Intent().apply {
