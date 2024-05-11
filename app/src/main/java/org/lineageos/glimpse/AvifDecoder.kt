63afd49a85d2c76afc81e9f6695b8009d60856ed
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-05-11 20:16:38 +0200

    Glimpse: AVIF decoder?
    Change-Id: I53427bd14d72ea657491e6115cdc85806a8229d7

---
+++ app/src/main/java/org/lineageos/glimpse/AvifDecoder.kt
@@ -0,0 +1,68 @@
+package org.lineageos.glimpse
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import coil.ImageLoader
+import coil.decode.DecodeResult
+import coil.decode.Decoder
+import coil.decode.ImageSource
+import coil.fetch.SourceResult
+import coil.request.Options
+import okio.BufferedSource
+import okio.ByteString.Companion.encodeUtf8
+import java.nio.ByteBuffer
+import org.aomedia.avif.android.AvifDecoder as NativeAvifDecoder
+
+class AvifDecoder @JvmOverloads constructor(
+    private val source: ImageSource,
+    private val options: Options,
+    private val enforceMinimumFrameDelay: Boolean = true
+) : Decoder {
+    override suspend fun decode(): DecodeResult {
+        val sourceBuffer = source.source().readByteArray()
+        val byteBuffer = ByteBuffer.wrap(sourceBuffer)
+        val info = NativeAvifDecoder.Info()
+        if (!NativeAvifDecoder.getInfo(byteBuffer, byteBuffer.remaining(), info)) {
+            throw IllegalArgumentException("Failed to get AVIF info.")
+        }
+        val bitmap = Bitmap.createBitmap(info.width, info.height, options.config)
+        if (!NativeAvifDecoder.decode(byteBuffer, byteBuffer.remaining(), bitmap)) {
+            throw IllegalArgumentException("Failed to decode AVIF.")
+        }
+        return DecodeResult(
+            drawable = BitmapDrawable(options.context.resources, bitmap),
+            isSampled = false
+        )
+    }
+
+    class Factory @JvmOverloads constructor(
+        private val enforceMinimumFrameDelay: Boolean = true
+    ) : Decoder.Factory {
+        override fun create(
+            result: SourceResult, options: Options, imageLoader: ImageLoader
+        ): Decoder? {
+            if (!isApplicable(result.source.source())) return null
+            return AvifDecoder(result.source, options, enforceMinimumFrameDelay)
+        }
+
+        private fun isApplicable(source: BufferedSource) =
+            AVAILABLE_BRANDS.any { source.rangeEquals(4, it) }
+
+        override fun equals(other: Any?) = other is Factory
+
+        override fun hashCode() = javaClass.hashCode()
+
+        companion object {
+            private val MIF = "ftypmif1".encodeUtf8()
+            private val MSF = "ftypmsf1".encodeUtf8()
+            private val HEIC = "ftypheic".encodeUtf8()
+            private val HEIX = "ftypheix".encodeUtf8()
+            private val HEVC = "ftyphevc".encodeUtf8()
+            private val HEVX = "ftyphevx".encodeUtf8()
+            private val AVIF = "ftypavif".encodeUtf8()
+            private val AVIS = "ftypavis".encodeUtf8()
+
+            private val AVAILABLE_BRANDS = listOf(MIF, MSF, HEIC, HEIX, HEVC, HEVX, AVIF, AVIS)
+        }
+    }
+}
