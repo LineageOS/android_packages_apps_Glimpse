358e69bc5ca6f4e0dbda3db434aa2aeccd6cb9dc
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-08-27 11:20:28 +0200

    Glimpse: Update material to 1.12.0
    Change-Id: Ifd17f45218d9b01729eecbd1868075c142176ca3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference:1.2.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     // EXIF
     implementation("androidx.exifinterface:exifinterface:1.3.7")
