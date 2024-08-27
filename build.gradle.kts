d5ed09ca47bd2d6510c85baea50c8d22b546e17e
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-08-27 11:10:33 +0200

    Glimpse: Update kotlin to 2.0.20
    Change-Id: I64720324d0e6a1a3d3dc35a5f056433fb7716720

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@
 plugins {
     id("com.android.application") version "8.5.2" apply false
     id("com.android.library") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
 
 tasks.register<Delete>("clean").configure {
