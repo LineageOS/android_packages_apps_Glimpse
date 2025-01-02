0bea5a75ba080516cd36a7a1176b16e024a10b57
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-01-02 01:23:27 +0100

    Glimpse: Bump gradle-generatebp to 1.18
    Change-Id: I3576debc3c79f13a69b0bdc9e35f15b11ec815ea

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ apply {
 
 buildscript {
     repositories {
-        maven("https://raw.githubusercontent.com/lineage-next/gradle-generatebp/v1.17/.m2")
+        maven("https://raw.githubusercontent.com/lineage-next/gradle-generatebp/v1.18/.m2")
     }
 
     dependencies {
