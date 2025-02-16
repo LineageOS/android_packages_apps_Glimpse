ec78e4660b4d1e202c50709be03116ed97a3b90c
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-02-16 15:24:53 +0100

    Glimpse: Split ACTION_REVIEW from ACTION_REVIEW_SECURE
    Change-Id: Icaa5df7bef3ab74de1c85c655db11cfd4ea57860

--- app/src/main/java/org/lineageos/glimpse/viewmodels/LocalPlayerViewModel.kt
+++ app/src/main/java/org/lineageos/glimpse/viewmodels/LocalPlayerViewModel.kt
@@ -123,6 +123,10 @@ class LocalPlayerViewModel(
 
                 is IntentsViewModel.ParsedIntent.ReviewIntent -> album
 
+                is IntentsViewModel.ParsedIntent.SecureReviewIntent -> flowOf(
+                    RequestStatus.Success(it.medias)
+                )
+
                 else -> flowOf(RequestStatus.Loading())
             }
         }
@@ -140,7 +144,7 @@ class LocalPlayerViewModel(
     val secure = parsedIntent
         .mapLatest {
             when (it) {
-                is IntentsViewModel.ParsedIntent.ReviewIntent -> it.secure
+                is IntentsViewModel.ParsedIntent.SecureReviewIntent -> true
                 else -> false
             }
         }
@@ -158,7 +162,8 @@ class LocalPlayerViewModel(
     val readOnly = parsedIntent
         .mapLatest {
             when (it) {
-                is IntentsViewModel.ParsedIntent.ReviewIntent -> it.secure
+                is IntentsViewModel.ParsedIntent.ReviewIntent,
+                is IntentsViewModel.ParsedIntent.SecureReviewIntent -> false
                 else -> true
             }
         }
