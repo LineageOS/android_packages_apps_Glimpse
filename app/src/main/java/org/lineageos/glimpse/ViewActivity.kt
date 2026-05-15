7ddf4598213e5e56a5fddfb7cd4e4bf42a350e50
Torsten Grote <t+lineageos2@grobox.de>
2026-05-15 12:18:57 -0300

    Ask for unlock, if trash or edit action invoked from lock screen
    Change-Id: Ie59712b44696a8cce85f285e33a710fe70ed4d17

--- app/src/main/java/org/lineageos/glimpse/ViewActivity.kt
+++ app/src/main/java/org/lineageos/glimpse/ViewActivity.kt
@@ -267,18 +267,22 @@ class ViewActivity : AppCompatActivity(R.layout.activity_view) {
 
         adjustButton.setOnClickListener {
             viewModel.displayedMedia.value?.let {
-                startActivity(
-                    Intent.createChooser(
-                        buildEditIntent(it),
-                        null
+                doOrUnlockFirst {
+                    startActivity(
+                        Intent.createChooser(
+                            buildEditIntent(it),
+                            null
+                        )
                     )
-                )
+                }
             }
         }
 
         deleteButton.setOnClickListener {
             viewModel.displayedMedia.value?.let {
-                trashMedia(it)
+                doOrUnlockFirst {
+                    trashMedia(it)
+                }
             }
         }
 
